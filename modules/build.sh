5048975e2e4a9fff53cd8b8531e82d4288d9a9b5
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-11-30 20:54:42 +0100

    Fixed build script

--- modules/build.sh
+++ modules/build.sh
@@ -5,7 +5,7 @@ npm i
 npm run build
 mv build ../../static/new-editor
 
-cd ../acnl-pattern
+cd ../ACNLPatternTool
 npm i
 sed -i 's/  publicPath: "\/"/  publicPath: "\/pattern-editor\/"/' config/webpack.config.js
 npm run build
